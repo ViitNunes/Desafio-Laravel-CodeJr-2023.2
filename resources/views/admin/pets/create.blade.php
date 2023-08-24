d5fa2995543d9726f9d43683a34142f97af422af
ViitNunes <vitorianunes313@gmail.com>
2023-08-23 23:38:57 -0300

    modifica views

--- resources/views/admin/pets/create.blade.php
+++ resources/views/admin/pets/create.blade.php
@@ -6,32 +6,7 @@
         @slot('title', 'Cadastrar Pet')
         @slot('url', route('pets.store'))
         @slot('form')
-            <div class="row">
-                <div class="form-group col-sm-12 col-md-4">
-                    <label for="name" class="required">Nome </label>
-                    <input type="text" name="name" id="name" autofocus class="form-control" required value="{{ old('name', $pet->name) }}">
-                </div>
-                <div class="form-group col-sm-12 col-md-4">
-                    <label for="species" class="required">Espécie </label>
-                    <input type="text" name="species" id="species" autofocus class="form-control" required value="{{ old('species', $pet->species) }}">
-                </div>
-                <div class="form-group col-sm-12 col-md-4">
-                    <label for="breed" class="required">Raça </label>
-                    <input type="text" name="breed" id="breed" class="form-control" required value="{{ old('breed', $pet->breed) }}">
-                </div>
-                <div class="form-group col-sm-12 col-md-4">
-                    <label for="owner_id" class="required">Dono </label>
-                    <input type="int" name="owner_id" id="owner_id" class="form-control" required value="{{ old('email', $pet->owner_id) }}">
-                </div>
-                <div class="form-group col-sm-12 col-md-4">
-                    <label for="birth_date" class="birth_date">Data de Nascimento </label>
-                    <input type="date" name="birth_date" id="birth_date" class="form-control" required value="{{ old('email', $pet->birth_date) }}">
-                </div>
-                <div class="form-group col-sm-12 col-md-4">
-                    <label for="treatments_done" class="required">Tratamentos Realizados </label>
-                    <input type="text" name="treatments_done" id="treatments_done" class="form-control" required value="{{ old('email', $pet->treatments_done) }}">
-                </div>
-            </div>
+            @include('admin.pets.form')
         @endslot
     @endcomponent
 
--- resources/views/admin/pets/form.blade.php
+++ resources/views/admin/pets/form.blade.php
@@ -4,11 +4,23 @@
         <input type="text" name="name" id="name" autofocus class="form-control" required value="{{ old('name', $pet->name) }}">
     </div>
     <div class="form-group col-sm-12 col-md-4">
-        <label for="email" class="required">E-mail </label>
-        <input type="email" name="email" id="email" class="form-control" required value="{{ old('email', $pet->id) }}">
+        <label for="species" class="required">Espécie </label>
+        <input type="text" name="species" id="species" autofocus class="form-control" required value="{{ old('species', $pet->species) }}">
     </div>
     <div class="form-group col-sm-12 col-md-4">
-        <label for="password" class="required">Senha </label>
-        <input type="password" name="password" id="password" class="form-control" required>
+        <label for="breed" class="required">Raça </label>
+        <input type="text" name="breed" id="breed" class="form-control" required value="{{ old('breed', $pet->breed) }}">
+    </div>
+    <div class="form-group col-sm-12 col-md-4">
+        <label for="owner_id" class="required">Dono </label>
+        <input type="int" name="owner_id" id="owner_id" class="form-control" required value="{{ old('owner_id', $pet->owner_id) }}">
+    </div>
+    <div class="form-group col-sm-12 col-md-4">
+        <label for="birth_date" class="required">Data de Nascimento </label>
+        <input type="date" name="birth_date" id="birth_date" class="form-control" required value="{{ old('email', $pet->birth_date) }}">
+    </div>
+    <div class="form-group col-sm-12 col-md-4">
+        <label for="treatments_done" class="required">Tratamentos Realizados </label>
+        <input type="text" name="treatments_done" id="treatments_done" class="form-control" required value="{{ old('email', $pet->treatments_done) }}">
     </div>
 </div>
