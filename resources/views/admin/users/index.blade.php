aabfbcb0830761f5f9de81a0fcd67af368cf15ec
ViitNunes <vitorianunes313@gmail.com>
2023-08-25 18:37:13 -0300

    modifica view index de usuarios

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -12,17 +12,17 @@
         @slot('body')
             @foreach ($users as $user)
                 <tr>
-                    <td>{{ $users->name }}</td>
-                    <td>{{ $users->email }}</td>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
                     <td class="options d-flex justify-content-center gap-1">
-                        <a href="{{ route('users.show', $users->id) }}" class="btn btn-dark">
+                        <a href="{{ route('users.show', $user->id) }}" class="btn btn-dark">
                             <i class="fas fa-search"></i>
                         </a>
 
-                        <a href="{{ route('users.edit', $users->id) }}" class="btn btn-dark"><i class="fas fa-pen"></i></a>
+                        <a href="{{ route('users.edit', $user->id) }}" class="btn btn-dark"><i class="fas fa-pen"></i></a>
     
                         
-                        <form class="form-delete d-inline-block" action="{{ route('users.destroy', $users->id) }}" method="post">
+                        <form class="form-delete d-inline-block" action="{{ route('users.destroy', $user->id) }}" method="post">
                             @csrf
                             @method('delete')
                             <button type="submit" class="btn btn-dark"><i class="fas fa-trash"></i></button>
@@ -33,6 +33,3 @@
         @endslot
     @endcomponent
 @endsection
-
-
-
