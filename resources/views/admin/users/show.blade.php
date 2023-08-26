d43bee55f9039568903e269bf0564e1cb717f71b
ViitNunes <vitorianunes313@gmail.com>
2023-08-25 21:42:12 -0300

    modifica view usuarios

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -7,7 +7,7 @@
             @include('admin.users.form')
         @endslot
         @slot('back')
-            <a href="{{ route('users.edit', $users->id) }}" class="btn btn-dark float-right ml-1"><i class="fas fa-pen"></i> Editar</a>
+            <a href="{{ route('users.edit', $user->id) }}" class="btn btn-dark float-right ml-1"><i class="fas fa-pen"></i> Editar</a>
             <a href="{{ route('users.index') }}" class="btn btn-dark float-right"><i class="fas fa-undo-alt"></i> Voltar</a>
         @endslot
     @endcomponent
