7505d0aeae3ca241d6294075f36bbbd75d0952c3
ViitNunes <vitorianunes313@gmail.com>
2023-09-09 16:32:42 -0300

    modifica UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,10 +18,15 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $this->faker->word(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'birth_date' => $this->faker->date(),
+            'work_time' => $this->faker->date(),
+            'address' => $this->faker->address(),
+            'phone' => $this->faker->phoneNumber(),
+            'office' => $this->faker->office(),
+            'password' => '123456', // password
             'remember_token' => Str::random(10),
         ];
     }
