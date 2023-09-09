9d876682ec9a29a0e04b50a2217cfb559183c344
ViitNunes <vitorianunes313@gmail.com>
2023-09-09 11:59:16 -0300

    adiciona caminho do ConsultationController

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PetController;
 use App\http\Controllers\UserController; 
 use App\http\Controllers\OwnerController;
+use App\http\Controllers\ConsultationController;
 
 /*
 |--------------------------------------------------------------------------
