dc95c97da045f1a68e5240748f0272ff5b69e322
ViitNunes <vitorianunes313@gmail.com>
2023-09-09 14:02:08 -0300

    adiciona model consultation

--- app/Models/Consultation.php
+++ app/Models/Consultation.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Consultation extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'pet_id',
+        'start_time',
+        'end_time',
+        'cost'
+    ];
+
+    public function pet() {
+    return $this->belongsTo(Pet::class, 'pet_id');
+    }
 }
